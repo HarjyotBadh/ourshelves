580da8a70e7f2689599a1585fba2a4e3d64d4747
HarjyotBadh <88450888+HarjyotBadh@users.noreply.github.com>
2024-09-17 00:03:51 -0400

    added env variable for firebaseConfig

---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+    apiKey: Constants.expoConfig.extra.firebaseApiKey,
+    authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
+    projectId: Constants.expoConfig.extra.firebaseProjectId,
+    storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
+    messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
+    appId: Constants.expoConfig.extra.firebaseAppId,
+    measurementId: Constants.expoConfig.extra.firebaseMeasurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { db, auth };
