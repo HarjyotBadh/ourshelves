47ba88e29d70f9893a78157719f04b578717e6e3
LukeL25 <lawsonluke.business@gmail.com>
2024-09-25 23:00:54 -0400

    Finished UI for profile selection page

--- app/profile-icons.tsx
+++ app/profile-icons.tsx
@@ -1,26 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { db } from "firebaseConfig";
-import { ExternalLink } from '@tamagui/lucide-icons'
 import { Link, Tabs } from 'expo-router'
-import { Anchor, ScrollView, Text, XStack, YStack, SizableText, Image } from 'tamagui'
+import { Avatar, Text, XStack, YStack, SizableText, Image } from 'tamagui'
 import { ToastControl } from 'app/CurrentToast'
-import { FlatList, ImageBackground, Pressable } from 'react-native'
+import { TouchableOpacity, ScrollView } from 'react-native'
 import { collection, getDocs, doc, getDoc, updateDoc } from 'firebase/firestore';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 // TODO - figure out how to change the header names and (tabs) back button
 
-// Data for profile page to be queried from db
-interface ProfileIcons {
-  profileIcons: string[];
-}
 
-export default function TabTwoScreen() {
+export default function IconGallery() {
   const [loading, setLoading] = useState(true);
-  const [ProfileIcons, setProfileIcons] = useState<ProfileIcons | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const profileId = "UserIcons"; // Placeholder ProfilePage doc id
-
+  const profileId = "profileIcons"; // Placeholder ProfilePage doc id
+  const [icons, setIconArray] = useState([]);
 
   // Greabbing the data about profile page from database
   useEffect(() => {
@@ -31,17 +25,14 @@ export default function TabTwoScreen() {
 
         // Fetch icon data
         if (profileId) {
-          const profileIconDocRef = doc(db, 'ProfileIcons', profileId);
+          const profileIconDocRef = doc(db, 'GlobalSettings', profileId);
           const profileIconDoc = await getDoc(profileIconDocRef);
           if (profileIconDoc.exists()) {
             const profileIconData = profileIconDoc.data();
-            setProfileIcons({
-              profileIcons: profileIconData.Icons
-            });
+            setIconArray(profileIconData.icons)
           } else {
             throw new Error('User not found');
           }
-          console.log(ProfileIcons?.profileIcons[0])
         } else {
           throw new Error('User not authenticated');
         }
@@ -56,52 +47,28 @@ export default function TabTwoScreen() {
     
   // For when an icon is selected
   const iconSelect = () => {
-
+    alert("It Works!")
   };
 
+  // Loading page 
   if (loading) {
     return <Text>Loading...</Text>;
   }
 
   return (
-    
-    <SafeAreaView>
-      <Pressable onPress={() => alert("this works!")}>
-          <Image
-              // Eventually make this a set number of predefined icons
-              source={{ width: 100, height: 100, uri: ProfileIcons?.profileIcons[0] }}
-              width="53%"
-              height="22%"
-          />
-        </Pressable>
-      <ScrollView
-      maxHeight={700}
-      width="100%"
-      backgroundColor="$background"
-      padding="$"
-      borderRadius="$4"
-    >
-      <XStack flexWrap="wrap" alignItems="center" justifyContent="center">
-        {/* TODO 
-        - Ask how to implement this so we go back a page 
-        */}
-        <Pressable onPress={() => alert("this works!")}>
-          <Image
-              // Eventually make this a set number of predefined icons
-              source={{ width: 100, height: 100, uri: ProfileIcons?.profileIcons[0] }}
-              width="53%"
-              height="22%"
-          />
-        </Pressable>
-        {/*<Circle margin="$4" size={120} backgroundColor="$orange9" />
-        <Square margin="$4" size={120} backgroundColor="$yellow9" />
-        <Circle margin="$4" size={120} backgroundColor="$green9" />
-        <Square margin="$4" size={120} backgroundColor="$blue9" />
-        <Circle margin="$4" size={120} backgroundColor="$purple9" />
-        <Square margin="$4" size={120} backgroundColor="$pink9" />
-        <Circle margin="$4" size={120} backgroundColor="$red9" />*/}
-      </XStack>
+    <ScrollView>
+      <YStack ai="center" gap="$4" px="$4" py="$4">
+        {icons.map((iconUri, index) => (
+          <TouchableOpacity key={index} onPress={() => iconSelect()}>
+            <Avatar circular size="$15">
+              <Avatar.Image
+                accessibilityLabel="ProfilePic"
+                src={iconUri}/>
+              <Avatar.Fallback backgroundColor="$blue10" />
+            </Avatar>
+          </TouchableOpacity>
+        ))}
+      </YStack>
     </ScrollView>
-  </SafeAreaView>
-  )
+  );
 }
