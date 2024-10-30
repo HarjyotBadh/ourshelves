3c1721fe938f56c685bbdb69c5a08c845555c267
LukeL25 <lawsonluke.business@gmail.com>
2024-10-30 14:21:11 -0400

    Fixed stuff

--- app/(tabs)/profile_page.tsx
+++ app/(tabs)/profile_page.tsx
@@ -78,7 +78,7 @@ export default function ProfilePage() {
             setCheckedTags({
               closeCommunity: profilePageData.tags.includes("closeCommunity"),
               zanyShenanigans: profilePageData.tags.includes("zanyShenanigans"),
-              familyFriendly: profilePageData.includes("familyFriendly"),
+              familyFriendly: profilePageData.tags.includes("familyFriendly"),
             });
 
           } else {
